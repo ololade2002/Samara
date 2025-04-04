c41bbedda7d5009b509ea348d00ed0c96a60ff32
ololade2002 <elejaololadee@gmail.com>
2025-04-05 00:19:16 +0100

    project setup with Next.js and TailwindCSS

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='f font-roboto'>Footer</div>
+  )
+}
+
+export default Footer
---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='
+    font-raleway'>Navbar</div>
+  )
+}
+
+export default Navbar
